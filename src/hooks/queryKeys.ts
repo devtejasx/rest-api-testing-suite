7593e1b9e10261e2639e8f2f523f4e73f7501a1d
devtejasx <tejasnagmote520@gmail.com>
2026-07-18 23:35:58 +0530

    feat(frontend): add execution query keys

--- src/hooks/queryKeys.ts
+++ src/hooks/queryKeys.ts
@@ -5,6 +5,8 @@ export const queryKeys = {
   collection: (id: string) => ["collections", id] as const,
   reports: ["reports"] as const,
   report: (id: string) => ["reports", id] as const,
+  executions: ["executions"] as const,
+  execution: (id: string) => ["executions", id] as const,
   workflows: ["cicd", "workflows"] as const,
   containers: ["docker", "containers"] as const,
 };
